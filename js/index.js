7bdba51cbec37530d5c5b9a16cd4720a9bd5c1f7
unknown <adrianmolina39@gmail.com>
2021-11-08 21:59:00 -0400

    first

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+  });
+
+  
+  var instance = M.Slider.getInstance(elem);
+
+
+
+
